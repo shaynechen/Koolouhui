d63b94e23da9bfa5b07342ddce7029206127bd13
michael.chai <cp612sh@gmail.com>
2014-07-25 00:01:30 +0800

    project initialized

---
+++ Koolouhui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Koolouhui.Startup))]
+namespace Koolouhui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
