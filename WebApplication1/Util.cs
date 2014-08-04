65a46273d253b3534aa0a40153f17ef1f9eaccf9
michael.chai <cp612sh@gmail.com>
2014-08-04 12:01:58 +0800

    ICP

---
+++ WebApplication1/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Koo.Web
+{
+    public static class Util
+    {
+        public static string RemoveLeft(string str, int count)
+        {
+
+            if (string.IsNullOrEmpty(str.Trim()) || str.Count() < count)
+            {
+                return string.Empty;
+            }
+
+            return (string)str.Substring(0, count);
+
+        }
+    }
+}
